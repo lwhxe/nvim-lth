0928f8bbaf80a2c0a7f598d55c9735accd52514c
lwhxe <alexanderthomasbohman8@gmail.com>
2026-09-24 17:08:14 +0100

    math support

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -1,14 +1,20 @@
 return {
-  "lervag/vimtex",
-  lazy = false,
-  ft = { "tex", "latex" },
-  init = function()
-    vim.g.vimtex_view_method = "zathura"
-    vim.g.vimtex_compiler_method = "latexmk"
-    vim.g.vimtex_compiler_latexmk = {
-      out_dir = "build",
-      continuous = 1,
-    }
-    vim.g.vimtex_view_forward_search_on_start = false
-  end,
+    "lervag/vimtex",
+    lazy = false,
+    init = function()
+        vim.g.vimtex_view_method = "zathura"
+        vim.g.vimtex_compiler_method = "latexmk"
+        vim.g.vimtex_compiler_latexmk = {
+            out_dir = "build",
+            continuous = 1,
+            options = {
+                "-shell-escape",
+                "-verbose",
+                "-file-line-error",
+                "-synctex=1",
+                "-interaction=nonstopmode",
+            },
+        }
+        vim.g.vimtex_view_forward_search_on_start = false
+    end,
 }
